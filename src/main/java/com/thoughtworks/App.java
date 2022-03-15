14c7f7dc6aaddba53dcfc85197beaa80bc65791e
xiaofei_global <xiaofei_global@163.com>
2022-03-15 22:27:18 +0800

    update args

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -6,7 +6,7 @@ package com.thoughtworks;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] rrr )
     {
         System.out.println( "I love china" );
     }
