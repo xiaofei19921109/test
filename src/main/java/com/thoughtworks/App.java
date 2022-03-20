d3aa357303510f0f72e4f6a1e69f1654a38b073f
xiaofei_global <xiaofei_global@163.com>
2022-03-20 11:41:52 +0800

    123456

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-      System.out.println("create merge request!");
+      System.out.println("create merge request!123456");
     }
 }
 
